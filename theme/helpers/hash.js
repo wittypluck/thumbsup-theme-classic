a6e6df15b6500d4bffeca1bade45ce515af59380
wittypluck <wittypluck@users.noreply.github.com>
2024-10-05 18:39:45 +0200

    customzations

---
+++ theme/helpers/hash.js
@@ -0,0 +1,11 @@
+module.exports = name => {
+  let hash = 0;
+  for (let i = 0; i < name.length; i++) {
+    const char = name.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+  }
+  // Convert to 32bit unsigned integer in base 36 and pad with "0" to ensure length is 7.
+  return (hash >>> 0).toString(36).padStart(7, '0');
+
+}
+
